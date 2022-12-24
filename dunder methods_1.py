96d2373524f7dbfb35d2ebb04508118087b4f0b9
Sam <106349608+Sam-4444@users.noreply.github.com>
2022-12-24 07:10:51 -0800

    Update and rename dunder methods(__add__  __init__  __str__).py to dunder methods_1.py

--- dunder methods_1.py
+++ dunder methods_1.py
@@ -1,4 +1,3 @@
-
 class a:
     def __init__(self, name, age):
         self.name = name
@@ -8,11 +7,16 @@ class a:
         return f"this is the __str__ method ! {self.name}"
 
     def __add__(self, other):
-        return f"this is __add__ method ! {self.age+other.age}"
+        return f"this is __add__ method ! {self.age + other.age}"
+
+    def __getattr__(self, item):
+        return f"this is __getattr__ method ! this item : \"{item}\" does not exist !"
 
 
 p1 = a("Sam ", 44)
 p2 = a("Max ", 22)
+p3 = a("master", 11)
 
 print(p1)
-print(p1+p2)
+print(p1 + p2)
+print(p3.lastName)
